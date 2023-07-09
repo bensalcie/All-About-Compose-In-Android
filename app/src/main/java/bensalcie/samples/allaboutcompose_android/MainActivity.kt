a283fc86adb85726bfd0b9d27740ede59fb41a39
bensalcie <bensalcie@gmail.com>
2023-07-09 16:05:13 +0300

    Compose Interoperability with Views System, Inflate layouts from xml and Use with compose

--- app/src/main/java/bensalcie/samples/allaboutcompose_android/MainActivity.kt
+++ app/src/main/java/bensalcie/samples/allaboutcompose_android/MainActivity.kt
@@ -2,75 +2,49 @@ package bensalcie.samples.allaboutcompose_android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import bensalcie.samples.allaboutcompose_android.ui.theme.AllAboutComposeAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            Column(modifier = Modifier.fillMaxHeight()) {
-                Image(painter = painterResource(id = R.drawable.humberger), contentDescription ="Hamburger photo" ,modifier = Modifier
-                    .height(300.dp)
-                    .fillMaxWidth(), contentScale = ContentScale.Crop)
-                    Spacer(modifier = Modifier.padding(15.dp))
-                Row(modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                    Text(text = "Happy mean Hamburger", style = TextStyle(fontSize = 24.sp))
-                    Text(text = "$5.99", style = TextStyle(fontSize = 24.sp, color = Color.Green))
-
-
-                }
-                Spacer(modifier = Modifier.padding(15.dp))
-
-                Text(text = "Happy mean Hamburger is now discounted for agiven amount of money and you should buy it.", style = TextStyle(fontSize = 20.sp), modifier = Modifier.padding(10.dp))
-
-                Spacer(modifier = Modifier.padding(15.dp))
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(14.dp).align(Alignment.CenterHorizontally)) {
-                    Text(text = "Order Now")
-                    
-                }
+        setContentView(R.layout.main_activity)
+        fragmentManager.beginTransaction()
+            .replace(R.id.main_container,RecipeListFragment())
+            .commit()
 
 
 
-            }
-        }
 
 
 
         //Hamburger task.
-
+        //Lessson 4
+//        setContent {
+//            Column(modifier = Modifier.fillMaxHeight()) {
+//                Image(painter = painterResource(id = R.drawable.humberger), contentDescription ="Hamburger photo" ,modifier = Modifier
+//                    .height(300.dp)
+//                    .fillMaxWidth(), contentScale = ContentScale.Crop)
+//                Spacer(modifier = Modifier.padding(15.dp))
+//                Row(modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+//                    Text(text = "Happy mean Hamburger", style = TextStyle(fontSize = 24.sp))
+//                    Text(text = "$5.99", style = TextStyle(fontSize = 24.sp, color = Color.Green))
+//
+//
+//                }
+//                Spacer(modifier = Modifier.padding(15.dp))
+//
+//                Text(text = "Happy mean Hamburger is now discounted for agiven amount of money and you should buy it.", style = TextStyle(fontSize = 20.sp), modifier = Modifier.padding(10.dp))
+//
+//                Spacer(modifier = Modifier.padding(15.dp))
+//                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(14.dp).align(Alignment.CenterHorizontally)) {
+//                    Text(text = "Order Now")
+//
+//                }
+//
+//
+//
+//            }
+//        }
 
 
         ///Basic Row Arrangment.
