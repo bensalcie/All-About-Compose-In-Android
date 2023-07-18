b382d4b252f16da50c6a4936ca547158fc79e6ff
bensalcie <bensalcie@gmail.com>
2023-07-18 15:58:41 +0300

    Jetpack compose learn how to implement pagination

--- app/src/main/java/bensalcie/samples/allaboutcompose_android/presentation/ui/recipe/recipelist/RecipeListViewModel.kt
+++ app/src/main/java/bensalcie/samples/allaboutcompose_android/presentation/ui/recipe/recipelist/RecipeListViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import bensalcie.samples.allaboutcompose_android.domain.model.Recipe
 import bensalcie.samples.allaboutcompose_android.repository.RecipeRepository
+import bensalcie.samples.allaboutcompose_android.utils.DEBUG_TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.delay
@@ -17,61 +18,110 @@ import javax.inject.Inject
 import javax.inject.Named
 
 ///Use ViewModelInject in the constructor to inject components to the viewmodel.
+
+const val PAGE_SIZE = 30
+
 @HiltViewModel
 class RecipeListViewModel @Inject constructor(
-                                              private val repository: RecipeRepository,
-                                              @Named("auth_token") private val token:String
-                                              ):ViewModel() {
+    private val repository: RecipeRepository,
+    @Named("auth_token") private val token: String
+) : ViewModel() {
 
 //Old model
     //  private  val _recipes :MutableLiveData<List<Recipe>> = MutableLiveData()
 //     val recipes : LiveData<List<Recipe>> get() = _recipes
 
-    val recipes : MutableState<List<Recipe>> = mutableStateOf(listOf())
+    val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
 
-    val selectedCategory :MutableState<FoodCategory?> = mutableStateOf(null)
+    val selectedCategory: MutableState<FoodCategory?> = mutableStateOf(null)
 
-    var categoryScrollPosition :Int = 0
+    var categoryScrollPosition: Int = 0
     var loading = mutableStateOf(false)
+    var page = mutableStateOf(1)
+    var recipeListScrolPosition = 0
 
     var query = mutableStateOf("")
-init {
-  newSearch()
-}
-     fun newSearch(){
-         loading.value = true
-         resetSearchState()
+
+    init {
+        newSearch()
+    }
+
+    fun newSearch() {
+        loading.value = true
+        resetSearchState()
         viewModelScope.launch {
             delay(2000)
 
-            val result =  repository.search(token = token, page = 1, query = query.value)
+            val result = repository.search(token = token, page = 1, query = query.value)
             recipes.value = result
-            loading.value= false
+            loading.value = false
 
         }
     }
 
-    fun onQueryChange(query:String){
-        this.query.value= query
+    fun onQueryChange(query: String) {
+        this.query.value = query
     }
 
-    fun onSelectedCategoryChanged(category:String){
+    fun onSelectedCategoryChanged(category: String) {
         val newCategory = getFoodCategory(category)
         selectedCategory.value = newCategory
         onQueryChange(category)
     }
-fun onChangeScrollPosition(position:Int){
-    this.categoryScrollPosition = position
-}
 
-    private fun resetSearchState(){
+    private fun incrementPage() {
+        page.value = page.value + 1
+    }
+
+
+     fun onChangeRecipeScrollPosition(position: Int) {
+        recipeListScrolPosition = position
+    }
+
+
+    //    Append new recipe list to the current recipe list.
+    private fun appendRecipes(recipes: List<Recipe>) {
+        val current = ArrayList(this.recipes.value)
+        current.addAll(recipes)
+        this.recipes.value = current
+    }
+
+     fun nextPage() {
+        viewModelScope.launch {
+            //Prevent duplication due to recomposition.
+            if ((recipeListScrolPosition + 1) >=  (page.value * PAGE_SIZE)) {
+                loading.value = true
+                incrementPage()
+                Log.d(DEBUG_TAG, "nextPage triggered: page ${page.value}")
+                //Just to show pagination api is fast.
+                delay(1000)
+                if (page.value > 1) {
+                    val results =
+                        repository.search(token = token, query = query.value, page = page.value)
+
+                    Log.d(DEBUG_TAG, "nextPage: RESULTS: $results")
+                    appendRecipes(results)
+                }
+                loading.value = false
+            }
+        }
+    }
+
+    fun onChangeScrollPosition(position: Int) {
+        this.categoryScrollPosition = position
+    }
+
+    private fun resetSearchState() {
         recipes.value = listOf()
-        if (selectedCategory.value?.value!=query.value)
+        page.value = 1
+        onChangeRecipeScrollPosition(0)
+        if (selectedCategory.value?.value != query.value)
             clearSelectedCategory()
 
     }
-    private fun clearSelectedCategory(){
-        this.selectedCategory.value =null
+
+    private fun clearSelectedCategory() {
+        this.selectedCategory.value = null
 
     }
 
