907816bf8251415f61f64f7321b26989400899d2
bensalcie <bensalcie@gmail.com>
2023-07-18 12:44:49 +0300

    Jetpack compose learn Fonting, Scaffolds,Snackbars

---
+++ app/src/main/java/bensalcie/samples/allaboutcompose_android/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package bensalcie.samples.allaboutcompose_android.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val AppShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(6.dp),
+    large = RoundedCornerShape(8.dp)
+
+
+)
