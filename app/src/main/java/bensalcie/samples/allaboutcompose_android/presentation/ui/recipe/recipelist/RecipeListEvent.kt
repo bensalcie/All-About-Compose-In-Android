62bea22f08145977928b6eeee518cd2911f07524
bensalcie <bensalcie@gmail.com>
2023-07-19 13:42:30 +0300

    Jetpack compose handle process death

--- app/src/main/java/bensalcie/samples/allaboutcompose_android/presentation/ui/recipe/recipelist/RecipeListEvent.kt
+++ app/src/main/java/bensalcie/samples/allaboutcompose_android/presentation/ui/recipe/recipelist/RecipeListEvent.kt
@@ -1,6 +1,10 @@
 package bensalcie.samples.allaboutcompose_android.presentation.ui.recipe.recipelist
 
 sealed class RecipeListEvent {
-    object NewSearchEvent: RecipeListEvent()
-    object NextPageEvent:RecipeListEvent()
+    object NewSearchEvent : RecipeListEvent()
+    object NextPageEvent : RecipeListEvent()
+
+    // Restore after process death.
+    object RestoreStateEvent : RecipeListEvent()
+
 }
--- app/src/main/java/bensalcie/samples/allaboutcompose_android/presentation/ui/recipe/recipelist/RecipeListViewModel.kt
+++ app/src/main/java/bensalcie/samples/allaboutcompose_android/presentation/ui/recipe/recipelist/RecipeListViewModel.kt
@@ -5,11 +5,14 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import bensalcie.samples.allaboutcompose_android.domain.model.Recipe
 import bensalcie.samples.allaboutcompose_android.repository.RecipeRepository
 import bensalcie.samples.allaboutcompose_android.utils.DEBUG_TAG
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.delay
@@ -21,11 +24,16 @@ import javax.inject.Named
 ///Use ViewModelInject in the constructor to inject components to the viewmodel.
 
 const val PAGE_SIZE = 30
+const val STATE_KEY_PAGE = "recipe.state.page.key"
+const val STATE_KEY_QUERY ="recipe.state.query.key"
+const val STATE_KEY_LIST_POSITION ="recipe.state.query.list_position"
+const val STATE_KEY_SELECTED_CATEGORY ="recipe.state.query.selected_category"
 
 @HiltViewModel
 class RecipeListViewModel @Inject constructor(
     private val repository: RecipeRepository,
-    @Named("auth_token") private val token: String
+    @Named("auth_token") private val token: String,
+      private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
 //Old model
@@ -44,7 +52,20 @@ class RecipeListViewModel @Inject constructor(
     var query = mutableStateOf("")
 
     init {
-        onTriggerEvent(RecipeListEvent.NewSearchEvent)
+        savedStateHandle.get<Int>(STATE_KEY_PAGE)?.let { p->setPage(p) }
+        savedStateHandle.get<String>(STATE_KEY_QUERY)?.let { q->setQuery(q) }
+
+        savedStateHandle.get<Int>(STATE_KEY_LIST_POSITION)?.let { p->setListScrollPosition(p) }
+
+        savedStateHandle.get<FoodCategory>(STATE_KEY_SELECTED_CATEGORY)?.let { c->setSelectedCategory(c) }
+
+        if(recipeListScrolPosition!=0){
+            onTriggerEvent(RecipeListEvent.RestoreStateEvent)
+
+        }else{
+            onTriggerEvent(RecipeListEvent.NewSearchEvent)
+
+        }
     }
 
     private suspend fun newSearch() {
@@ -60,22 +81,23 @@ class RecipeListViewModel @Inject constructor(
     }
 
     fun onQueryChange(query: String) {
-        this.query.value = query
+        setQuery(query = query)
     }
 
     fun onSelectedCategoryChanged(category: String) {
         val newCategory = getFoodCategory(category)
-        selectedCategory.value = newCategory
+        setSelectedCategory(newCategory)
         onQueryChange(category)
     }
 
     private fun incrementPage() {
-        page.value = page.value + 1
+        setPage(page.value+1)
+
     }
 
 
      fun onChangeRecipeScrollPosition(position: Int) {
-        recipeListScrolPosition = position
+        setListScrollPosition(position = position)
     }
 
 
@@ -100,6 +122,10 @@ class RecipeListViewModel @Inject constructor(
                         nextPage()
 
                     }
+                    is RecipeListEvent.RestoreStateEvent->{
+                        restoreState()
+
+                    }
                 }
 
             }catch (e:Exception){
@@ -108,14 +134,34 @@ class RecipeListViewModel @Inject constructor(
         }
     }
 
-   private suspend  fun nextPage() {
+    private suspend fun restoreState() {
+        loading.value = true
+        val results : MutableList<Recipe> =  mutableListOf()
+
+        for (p in 1..page.value){
+
+            val result = repository.search(token= token, page = p, query = query.value)
+
+            results.addAll(result)
+
+            if(p==page.value){
+                //done
+                recipes.value = results
+                loading.value = false
+            }
+
+        }
+
+
+
+    }
+
+    private suspend  fun nextPage() {
             //Prevent duplication due to recomposition.
             if ((recipeListScrolPosition + 1) >=  (page.value * PAGE_SIZE)) {
                 loading.value = true
                 incrementPage()
                 Log.d(DEBUG_TAG, "nextPage triggered: page ${page.value}")
-                //Just to show pagination api is fast.
-                delay(1000)
                 if (page.value > 1) {
                     val results =
                         repository.search(token = token, query = query.value, page = page.value)
@@ -142,9 +188,29 @@ class RecipeListViewModel @Inject constructor(
     }
 
     private fun clearSelectedCategory() {
-        this.selectedCategory.value = null
+        setSelectedCategory(null)
 
     }
+    private fun setListScrollPosition(position: Int){
+        recipeListScrolPosition = position
+        savedStateHandle[STATE_KEY_LIST_POSITION] = position
+    }
+    private fun setPage(page:Int){
+        this.page.value = page
+        savedStateHandle[STATE_KEY_PAGE] = page
+    }
+
+    private fun setSelectedCategory(category:FoodCategory?){
+        selectedCategory.value = category
+        savedStateHandle[STATE_KEY_SELECTED_CATEGORY] = category
+
+    }
+
+    private fun setQuery(query:String){
+        this.query.value = query
+        savedStateHandle[STATE_KEY_QUERY] = query
+    }
+
 
 //    init {
 //
